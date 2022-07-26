fc8f2fb46e3b3fd5015c5b31b9f1d4aa5daa1bf2
Alexander Massuda <alexcaiom>
2022-07-26 01:03:53 -0300

    .

--- src/main/java/com/bank/debtrenegociation/service/impl/AgreementServiceImpl.java
+++ src/main/java/com/bank/debtrenegociation/service/impl/AgreementServiceImpl.java
@@ -26,7 +26,6 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Service
 @Slf4j
-
 public class AgreementServiceImpl implements AgreementService {
 	
 	@Autowired private AgreementProposalRepository repository;
